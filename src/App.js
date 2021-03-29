6be082c5bad4caf60e2f2152bde94fdd7083415c
nmoragane <nmoragane@gmail.com>
2021-03-29 15:36:00 +0530

    added all rows

--- src/App.js
+++ src/App.js
@@ -7,6 +7,12 @@ function App() {
     <div className="App">
       <Rows title = "Netflix Originals" fetchUrl={requests.fetchNetflixOriginals}/>   
       <Rows title = "Trending Now" fetchUrl={requests.fetchTrending}/>   
+      <Rows title = "Top Rated" fetchUrl={requests.fetchTopRated}/>   
+      <Rows title = "Action Movies" fetchUrl={requests.fetchActionMovies}/>   
+      <Rows title = "Comedy Movies" fetchUrl={requests.fetchComedyMovies}/>   
+      <Rows title = "Horror Movies" fetchUrl={requests.fetchHorrorMovies}/>   
+      <Rows title = "Romanctic Movies" fetchUrl={requests.fetchRomanceMovies}/>   
+      <Rows title = "Documentaries" fetchUrl={requests.fetchDocumentaries}/>   
     </div>
   );
 }
